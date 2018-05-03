6792c9dc49c946aeedcd9a360c08f2ff2e2a38c2
Marty McFly <pascal.gilcher@gmx.de>
2018-05-03 15:34:55 +0000

    Improve volumetric rays: add dithering, restructure code for more performance, improve sun blend

--- gtaRenderHook/shaders/VolumetricLighting.hlsl
+++ gtaRenderHook/shaders/VolumetricLighting.hlsl
@@ -17,8 +17,8 @@ SamplerComparisonState samShadow : register(s1);
 
 struct PS_QUAD_IN
 {
-    float4 vPosition : SV_Position;
-    float4 vTexCoord : TEXCOORD0;
+        float4 vPosition : SV_Position;
+        float4 vTexCoord : TEXCOORD0;
 };
 /*!
     Calculates sun-light volumetric scattering
@@ -26,73 +26,92 @@ struct PS_QUAD_IN
 */
 float3 ComputeSunRays(float3 ViewPos, float3 WorldPos, float Length)
 {
-    const int SunRaySampleCount = SUNLIGHT_RM_STEPS;
-    float3 Step = normalize(WorldPos - ViewPos) * Length / (float) SunRaySampleCount;
-    float3 CurrentPos = ViewPos;
-    float3 ResultColor = float3(0, 0, 0);
-    for (int i = 0; i < SunRaySampleCount; i++)
-    {
-        ResultColor += SampleShadowCascadesUnfiltered(txShadow, samShadow, samLinear, CurrentPos, (Length / (float) SunRaySampleCount) * i);
-        CurrentPos += Step;
-    }
-    return ResultColor / (float) SunRaySampleCount;
+        const int SunRaySampleCount = SUNLIGHT_RM_STEPS;
+        float3 Step = normalize(WorldPos - ViewPos) * Length / (float)SunRaySampleCount;
+        float3 CurrentPos = ViewPos;
+        float3 ResultColor = float3(0, 0, 0);
+        for(int i = 0; i < SunRaySampleCount; i++)
+        {
+                ResultColor += SampleShadowCascadesUnfiltered(txShadow, samShadow, samLinear, CurrentPos, (Length / (float)SunRaySampleCount) * i);
+                CurrentPos += Step;
+        }
+        return ResultColor / (float)SunRaySampleCount;
 }
 
 /*!
     Calculates sun-light volumetric scattering
     TODO: improve this alghorithm quality and performance, by introducing jittering or maybe something more
 */
-float3 ComputeSunRaysWithLighting(float3 LightDir, float3 ViewPos, float3 WorldPos, float Length)
+
+float3 ComputeSunRaysWithLighting(float2 vTexCoord, float2 vPosition, float3 LightDir, float3 ViewPos, float3 WorldPos, float Length)
 {
-    const int SunRaySampleCount = SUNLIGHT_RM_STEPS;
-    float3 ViewDir = normalize(WorldPos - ViewPos);
-    float3 Step = ViewDir * Length / (float) SunRaySampleCount;
-    float3 CurrentPos = ViewPos;
-    float SunRaysIntensity = min(max(dot(ViewDir, LightDir) + SunlightBlendOffset, 0.0f), 1.0f);
-    float3 ResultColor = float3(0, 0, 0);
-    for (int i = 0; i < SunRaySampleCount; i++)
-    {
-        ResultColor += SampleShadowCascadesUnfiltered(txShadow, samShadow, samLinear, CurrentPos, (Length / (float) SunRaySampleCount) * i) * (SunRaysIntensity * vSunColor.rgb * vSunColor.a * 0.5f + GetRayleighDensity(CurrentPos.z) * vSkyLightCol.rgb * 0.25f + GetMieDensity(CurrentPos.z) * vHorizonCol.rgb * 0.25f);
-        CurrentPos += Step;
-    }
-    return ResultColor / (float) SunRaySampleCount;
+        const int SunRaySampleCount = SUNLIGHT_RM_STEPS;
+        float3 ViewDir = normalize(WorldPos - ViewPos);
+        float StepLength = Length / (float)SunRaySampleCount;
+
+        float3 Step = ViewDir * StepLength;
+    //float Jitter =  frac(frac(dot(vPosition.xy, float2(0.06711056,0.00583715))) * 52.9829189); //interleaved gradient noise
+
+        static const float Bayer[16] = { 0, 12, 3, 15, 8, 4, 11, 7, 2, 14, 1, 13, 10, 6, 9, 5 };
+        int2 MatIndex = vPosition.xy % 4;
+        float Jitter = (Bayer[MatIndex.x + 4 * MatIndex.y] + 1) / 16.0;
+
+        float3 CurrentPos = ViewPos + Step * Jitter;
+        float3 ResultColor = 0.0;
+
+        float SunBlend = saturate(dot(ViewDir, LightDir) + SunlightBlendOffset);
+        SunBlend = SunBlend * SunBlend * 2.0;
+
+        float3 SunRayContrib = SunBlend * vSunColor.rgb * vSunColor.a * 0.5f;
+        static const float3 RayleighContrib = vSkyLightCol.rgb * 0.25f;
+        static const float3 MieContrib = vHorizonCol.rgb * 0.25f;
+
+        for(int i = 0; i < SunRaySampleCount; i++)
+        {
+                float3 CurrentRay = SampleShadowCascadesUnfiltered(txShadow, samShadow, samLinear, CurrentPos, StepLength * (i + Jitter));
+                ResultColor += CurrentRay * (SunRayContrib
+                                   + RayleighContrib * GetRayleighDensity(CurrentPos.z)
+                                   + GetMieDensity(CurrentPos.z) * MieContrib);
+                CurrentPos += Step;
+        }
+        return ResultColor / (float)SunRaySampleCount;
 }
 
 float4 VolumetricSunlightPS(PS_QUAD_IN i) : SV_TARGET
 {
-    float4 OutLighting;
+        float4 OutLighting;
 
-    const float3 ViewPos = mViewInv[3].xyz;
+        const float3 ViewPos = mViewInv[3].xyz;
 
     // Retrieve all needed buffer samples first
-    float ViewZ;
-    float3 Normals;
-    GetNormalsAndDepth(txGB1, samLinear, i.vTexCoord.xy, ViewZ, Normals);
+        float ViewZ;
+        float3 Normals;
+        GetNormalsAndDepth(txGB1, samLinear, i.vTexCoord.xy, ViewZ, Normals);
 
-    float3 WorldPos = DepthToWorldPos(ViewZ, i.vTexCoord.xy).xyz;
+        float3 WorldPos = DepthToWorldPos(ViewZ, i.vTexCoord.xy).xyz;
     
     // Directions calculation maybe we should introduce macroses to encapsulate them
-    float3 ViewDir = normalize(WorldPos - ViewPos);
-    float3 LightDir = normalize(vSunLightDir.xyz);
+        float3 ViewDir = normalize(WorldPos - ViewPos);
+        float3 LightDir = normalize(vSunLightDir.xyz);
 
-    float SunRaysIntensity = min(max(dot(ViewDir, LightDir) + SunlightBlendOffset, 0.0f), 1.0f) * SunlightIntensity;
+        float SunRaysIntensity = min(max(dot(ViewDir, LightDir) + SunlightBlendOffset, 0.0f), 1.0f) * SunlightIntensity;
 
-    float3 SunRays = ComputeSunRaysWithLighting(LightDir, ViewPos, WorldPos, min(length(WorldPos - ViewPos), RaymarchingDistance));
+        float3 SunRays = ComputeSunRaysWithLighting(i.vTexCoord.xy, i.vPosition.xy, LightDir, ViewPos, WorldPos, min(length(WorldPos - ViewPos), RaymarchingDistance));
 
-    OutLighting.rgb = SunRays * SunlightIntensity;
-    OutLighting.a = 1;
+        OutLighting.rgb = SunRays * SunlightIntensity;
+        OutLighting.a = 1;
 
-    return OutLighting;
+        return OutLighting;
 }
 
 float4 VolumetricCombinePS(PS_QUAD_IN i) : SV_TARGET
 {
-    float4 OutLighting;
+        float4 OutLighting;
 
-    float4 ScreenColor = txScreen.Sample(samLinear, i.vTexCoord.xy);
+        float4 ScreenColor = txScreen.Sample(samLinear, i.vTexCoord.xy);
     
-    OutLighting.rgb = ScreenColor.rgb + txVolumetric.Sample(samLinear, i.vTexCoord.xy).rgb;
-    OutLighting.a = 1;
+        OutLighting.rgb = ScreenColor.rgb + txVolumetric.Sample(samLinear, i.vTexCoord.xy).rgb;
+        OutLighting.a = 1;
 
-    return OutLighting;
+        return OutLighting;
 }
